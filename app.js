568456f9fb67287cd9e67b703ca499b3f50f5f39
marudaga <83997563+marudaga@users.noreply.github.com>
2021-07-06 04:14:56 +0900

    4.1

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const loginButton = loginForm.querySelector("button");
 // document는 html전체를  reference한다
 
 function onLoginBtnClick(){
+    const username = loginInput.value;
+    if(username ===""){
+        alert("Please write your name");
+    }
+    else if(username.length>15){
+        alert("Your name is too long.");
+    }
     console.log(loginInput.value);
     /*console.dir(loginInput) attribute중에서 value사용
     console.log하나 출력(""안밖상관없이 전체 출력됨) info 여러객체 출력("hello",b)=hello b의값,  
