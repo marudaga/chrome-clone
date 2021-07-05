038702be0062ee381de81c9fae546f594ac70c59
marudaga <83997563+marudaga@users.noreply.github.com>
2021-07-06 03:25:54 +0900

    4.0

---
+++ app.js
@@ -0,0 +1,14 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput  = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+// document는 html전체를  reference한다
+
+function onLoginBtnClick(){
+    console.log(loginInput.value);
+    /*console.dir(loginInput) attribute중에서 value사용
+    console.log하나 출력(""안밖상관없이 전체 출력됨) info 여러객체 출력("hello",b)=hello b의값,  
+    %.2d정수(%d정수출력) 10자리수까지 표현, %s 문자열,%.2f 소수점 2번째까지 출력 */
+    console.log("click!!!");
+}
+
+loginButton.addEventListener("click",onLoginBtnClick);
