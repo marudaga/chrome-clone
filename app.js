3db6b61cb54fe1a6a289bc53247b2f64348ca252
marudaga <83997563+marudaga@users.noreply.github.com>
2021-07-07 02:10:24 +0900

    4.6

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const greeting = document.querySelector("#greeting");
 
 const  HIDDEN_CLASSNAME = "hidden";
 
+const savedUsername = localStorage.getItem("username");
 /*document는 html전체를  reference한다
 "hello" + username 와 `hello ${username}` 같은표현이다 
 [''가 아니라 ``다]
@@ -17,15 +18,30 @@ classlist.add("hidden") classlist.remove("hidden") ""표시해야함
 /*4.5 localStorage에 user정보 저장하기가 가장 쉽다
  console 창에서 application 에서  localStorage.setItem("username", "nico")라고 입력하면 
  우리 Storage 항목에  들어가게된다.
+ localStorage.setItem("key",Value)가 된다.
  가져올때도 console창에 setitem 처럼 getitem이나 removeitem 가능*/
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem("username", username);
-    greeting.innerText ="hello " + username;
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
+    greeting.innerText ="Hello " + username;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+if(savedUsername ===null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else{
+    //show the greeting
+    paintGreetings(savedUsername);
+}
+console.log("savedUsername")
 
 loginForm.addEventListener("submit", onLoginSubmit)
