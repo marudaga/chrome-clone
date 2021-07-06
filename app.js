548abb06bda5cd8819ad822cd2a02d1f7755214f
marudaga <83997563+marudaga@users.noreply.github.com>
2021-07-07 01:49:23 +0900

    4.5

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const loginForm = document.querySelector("#login-form");
-<<<<<<< HEAD
 const loginInput  = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
@@ -15,32 +14,18 @@ classlist.add("hidden") classlist.remove("hidden") ""표시해야함
  대문자로 표기한다 중요하지않은정보이기 때문에
 */
 
+/*4.5 localStorage에 user정보 저장하기가 가장 쉽다
+ console 창에서 application 에서  localStorage.setItem("username", "nico")라고 입력하면 
+ 우리 Storage 항목에  들어가게된다.
+ 가져올때도 console창에 setitem 처럼 getitem이나 removeitem 가능*/
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem("username", username);
     greeting.innerText ="hello " + username;
     greeting.classList.remove(HIDDEN_CLASSNAME);
-
-=======
-const loginInput  = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
-// document는 html전체를  reference한다
-
-function onLoginBtnClick(){
-    const username = loginInput.value;
-    if(username ===""){
-        alert("Please write your name");
-    }
-    else if(username.length>15){
-        alert("Your name is too long.");
-    }
-    console.log(loginInput.value);
-    /*console.dir(loginInput) attribute중에서 value사용
-    console.log하나 출력(""안밖상관없이 전체 출력됨) info 여러객체 출력("hello",b)=hello b의값,  
-    %.2d정수(%d정수출력) 10자리수까지 표현, %s 문자열,%.2f 소수점 2번째까지 출력 */
-    console.log("click!!!");
->>>>>>> 67cf1c2dd9891618fc96bffbadd19811b7dc1edc
 }
+
 
 loginForm.addEventListener("submit", onLoginSubmit)
